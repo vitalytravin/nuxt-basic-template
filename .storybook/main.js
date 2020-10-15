d8b65ef4da5130ad54efd55b599764d6789fef76
Vitaly Travin <sputnik906@gmail.com>
2020-10-15 12:05:28 +0400

    build(storybook): add config for relative path

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,17 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-knobs"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    
+    config.resolve.alias['@']=path.resolve(__dirname, '../')
+
+
+    // Return the altered config
+    return config;
+  },
 }
